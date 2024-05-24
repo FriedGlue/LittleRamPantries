f5564e37783cdb6fe8610824c99bb0195f354465
Joshua Hayes <josh.hayes121@icloud.com>
2024-05-24 16:44:54 -0400

    navbar!

--- src/frontend/client/src/components/Navbar/Navbar.tsx
+++ src/frontend/client/src/components/Navbar/Navbar.tsx
@@ -1,40 +1,57 @@
 import { Link } from 'react-router-dom';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-
-import './Navbar.css';
 import LRPLogo from '../../assets/ram-pantry.png';
 import { SchoolConfig } from '../../types/types';
+import { useState } from 'react';
 
-function NavbarScroll(
-  { schoolConfig } :
-  { schoolConfig : SchoolConfig }
-) {
+function NavbarScroll({ schoolConfig }: { schoolConfig: SchoolConfig }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleNavbar = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const closeNavbar = () => {
+    setIsOpen(false);
+  };
+
   return (
-    <div style={{backgroundColor: schoolConfig.footer_color}}>
-    <Navbar sticky="top" expand="lg" >
-      <Container>
-        <Navbar.Brand as={Link} to="/" >
-          <img
-            alt="LRP logo"
-            src={LRPLogo}
-            width="30"
-            height="30"
-            className="d-inline-block align-top"
-          /> Little Ram Pantries
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav">
-          &#9660;
-        </Navbar.Toggle>
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link as={Link} to="/">Home</Nav.Link>
-            <Nav.Link href="https://news.vcu.edu/article/2021/10/little-ram-pantries-will-provide-emergency-food-assistance-to-vcu-students">Learn More</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <div style={{ backgroundColor: schoolConfig.footer_color }}>
+      <nav className={`sticky top-0 z-50 w-full ${isOpen ? 'bg-white' : ''}`}>
+        <div className="container mx-auto flex items-center justify-between p-4">
+          <Link to="/" className="flex items-center" onClick={closeNavbar}>
+            <img
+              alt="LRP logo"
+              src={LRPLogo}
+              width="30"
+              height="30"
+              className="mr-2"
+            />
+            <span className="text-lg font-semibold text-black pr-4">Little Ram Pantries</span>
+          </Link>
+          <div className="lg:hidden flex items-center">
+            <button
+              onClick={toggleNavbar}
+              className={`text-lg focus:outline-none ${isOpen ? 'hidden' : 'block'}`}
+            >
+              &#9660;
+            </button>
+            <button
+              onClick={closeNavbar}
+              className={`text-lg focus:outline-none ${isOpen ? 'block' : 'hidden'}`}
+            >
+              &#10005;
+            </button>
+          </div>
+          <div className={`lg:flex lg:items-center lg:static absolute top-full left-0 w-full lg:w-auto ${isOpen ? 'bg-white block' : 'hidden'}`}>
+            <div className="flex flex-col lg:flex-row lg:ml-auto">
+              <Link to="/" className="py-2 px-4 hover:bg-gray-200 text-gray-500" onClick={closeNavbar}>Home</Link>
+              <a href="https://news.vcu.edu/article/2021/10/little-ram-pantries-will-provide-emergency-food-assistance-to-vcu-students" className="py-2 px-4 hover:bg-gray-200 text-gray-500" onClick={closeNavbar}>
+                Learn More
+              </a>
+            </div>
+          </div>
+        </div>
+      </nav>
     </div>
   );
 }
