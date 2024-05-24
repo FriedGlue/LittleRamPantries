79bd892ed96c72a06fe3e0a0204c2cd85afe3f9f
Joshua Hayes <josh.hayes121@icloud.com>
2024-05-24 16:17:33 -0400

    error page!

--- src/frontend/client/src/routes/ErrorPage.tsx
+++ src/frontend/client/src/routes/ErrorPage.tsx
@@ -14,7 +14,7 @@ export default function ErrorPage() {
   }
 
   return (
-    <div id="error-page">
+    <div className='flex flex-col items-center justify-center min-h-[750px] w-full'>
       <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
       {<p><i>{statusCodeMessage}: {errorMessage}</i></p>}
